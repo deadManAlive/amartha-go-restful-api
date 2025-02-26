3b19bf7b98448541a994cfff369272fea0427346
unknown <alfarrakhan@gmail.com>
2025-02-26 15:10:09 +0700

    using wire

---
+++ sample/injector.go
@@ -0,0 +1,11 @@
+//go:build wireinject
+// +build wireinject
+
+package sample
+
+import "github.com/google/wire"
+
+func InitializeService() *SimpleService {
+	wire.Build(NewSimpleRepository, NewSimpleService)
+	return nil
+}
